195b5f8e62c89493ccece96d2eda85d1b4c848da
Shashwat Trivedi <shashwattrivedi6.5.97@gmail.com>
2019-04-09 22:13:03 +0530

    Added tooltip and format adjustment

--- attention_visualizer.py
+++ attention_visualizer.py
@@ -2,92 +2,102 @@ from IPython.core.display import display, HTML
 from string import Template
 import json
 
-html_template = Template('''<script src="https://d3js.org/d3.v4.min.js"></script>
+def display_attention(sentence,attention_weights,scale=0,offset=18,style=1):
+  html_template = Template('''
+  <script src="https://d3js.org/d3.v4.min.js"></script>
+  <style>$css_text</style>
+  <script>$js_text</script>
+  <div id = 'text' style="margin-left:50px;"></div>
+  ''')
 
-<style>$css_text</style>
-<script>$js_text</script>
-<div id = 'text'></div>''')
+  css_text = '''
+  html, body {
+      margin: 0;
+      padding: 0;
+  }
 
-css_text = '''html, body {
-    margin: 0;
-    padding: 0;
-}
-'''
+  .tooltip {
+    position: relative;
+    display: inline-block;
+    border-bottom: 1px dotted black;
+  }
+  .tooltip:hover .tooltiptext {
+    visibility: visible;
+  }
 
-js_template = Template('''
-var dataset =[["This",0.4],["is",0.3],["a",0.2],["text",0.4]]
-var seconddataset = ["Hello","World"]
+  .tooltip .tooltiptext {
+    visibility: hidden;
+    font-size:15px;
+    width: 60px;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 1px 0;
+    position: absolute;
 
-function float2int (value) {
-    return value | 0;
-}
+    left: 50%;
+    margin-left: -60px;
+  }
 
-function tohex(fraction)
-{
-  var value = float2int(256 * fraction);
-  var mapping = ['A','B','C','D','E','F'];
-  var hex="";
-  
-  while(value!==0)
-    {
-      var curr= value%16;
-      if(curr >9)
-        {
-          hex = mapping[curr-10] + hex;
-        }
-      else
-        hex = curr + hex;
-      value = float2int(value/16);
-    }
+  '''
 
-  return hex;
-}
+  js_template = Template('''
+  var dataset =[["This",0.4],["is",0.3],["a",0.2],["text",0.4]]
+  var seconddataset = ["Hello","World"]
 
-// Create Event Handlers for mouse
-function handleMouseOver(d, i) {  // Add interactivity
+  function float2int (value) {
+      return value | 0;
+  }
 
-      // Use D3 to select element, change color and size
-      d3.select(this).attr({
-        fill: "orange"
-      });
+  function tohex(fraction)
+  {
+    var value = float2int(255 * fraction);
+    if(value==0)
+      return "00"
+    var mapping = ['A','B','C','D','E','F'];
+    var hex="";
 
-      // Specify where to put label of text
-      svg.append("text").attr({
-         id: "t" + d.x + "-" + d.y + "-" + i,  // Create an id for text so we can select it later for removing on mouseout
-          x: function() { return xScale(d.x) - 30; },
-          y: function() { return yScale(d.y) - 15; }
-      })
-      .text(function() {
-        return [d.x, d.y];  // Value of the text
-      });
-    }
+    while(value!==0)
+      {
+        var curr= value%16;
+        if(curr >9)
+          {
+            hex = mapping[curr-10] + hex;
+          }
+        else
+          hex = curr + hex;
+        value = float2int(value/16);
+      }
 
+    return hex;
+  }
 
-d3.select('#text')
-.selectAll('#text')
-.data($data)
-.enter()
-.append('tspan')
-.style('font-family','verdana')
-$style
-.style('margin','2px')
-.style('font-size',function(d){return  $offset +$scale*d.weight + "px" ;})
-.text(function(d){
-  return d.token+" " ;
-});
+  d3.select('#text')
+  .selectAll('#text')
+  .data($data)
+  .enter()
+  .append('tspan')
+  .style('font-family','verdana')
+  $style
+  .style('margin','2px')
+  .attr("class","tooltip")
+  .style('font-size',function(d){return  $offset +$scale*d.weight + "px" ;})
+  .attr("onmouseover", "handleMouseOver()")
+  .text(function(d){
+    return d.token+" " ;
+  })
+  .append('span')
+  .attr('class',"tooltiptext")
+  .text(function(d){
+  return d.weight*100;
+  });
+  ''')
 
-
-
-''')
-
-style1 =".style('background-color', function(d,i){return '#FF' +tohex(1-d.weight) +tohex(1-d.weight) ;})"
-
-def display_attention(sentence,attention_weights,scale,offset,style=1):
+  style1 =".style('background-color', function(d,i){return '#FF' +tohex(1-d.weight) +tohex(1-d.weight) ;})"  
   data = [{'token':token, 'weight' :weight} for token,weight in zip(sentence,attention_weights)]
-  
-  if style ==1 :
-    js_text = js_template.substitute({'data' : json.dumps(data),'scale': 0, 'offset':20, 'style': style1})
-  else:
-    js_text = js_template.substitute({'data' : json.dumps(data),'scale': scale, 'offset':offset, 'style': ""})
+
+  js_text = js_template.substitute({'data' : json.dumps(data),'scale': 0 if style==1 else scale, 'offset':offset, 'style': style1 if style==1 else ""})
+
     
   return display(HTML(html_template.substitute({'css_text':css_text,'js_text':js_text})))
